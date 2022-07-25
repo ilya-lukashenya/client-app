1663184fe2862980632c192cce15f3f8513d3e2d
ilya-lukashenya <il.lukashenya@gmail.com>
2022-07-26 00:11:37 +0300

    Test

---
+++ src/app/layout/App.tsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import {Container, Header, List} from 'semantic-ui-react';
+import { Activity } from '../models/activity';
+import NavBar from './NavBar';
+
+function App() {
+  const [activities, setActivities] = useState<Activity[]>([]);
+
+  useEffect(() => {
+    axios.get<Activity[]>('http://localhost:5000/api/activities').then(response => { 
+    setActivities(response.data);
+    })
+  }, [])
+
+  return (
+    <div>
+      <NavBar />
+      <Container style = {{marfinTop: '7em'}}>
+      <List>
+      {activities.map(activity => (
+            <List.Item key = {activity.id}>
+              {activity.title}
+            </List.Item>
+          ))}
+      </List>
+      </Container>
+      
+    </div>
+  );
+}
+
+export default App;
