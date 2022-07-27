8a20d8ef4e97da987b425fca4d12c32d8687850e
ilya-lukashenya <il.lukashenya@gmail.com>
2022-07-27 04:34:40 +0300

    Section 5 without image.

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -1,11 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import React, {  useEffect, useState } from 'react';
 import axios from 'axios';
-import {Container, List} from 'semantic-ui-react';
+import {Container} from 'semantic-ui-react';
 import { Activity } from '../models/activity';
 import NavBar from './NavBar';
+import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
+import {v4 as uuid} from 'uuid';
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
+  const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
+  const [editMode, setEditMode] = useState(false);
 
   useEffect(() => {
     axios.get<Activity[]>('http://localhost:5000/api/activities').then(response => { 
@@ -13,20 +17,52 @@ function App() {
     })
   }, [])
 
+  function handleSelectActivity(id: string) {
+    setSelectedActivity(activities.find(x => x.id === id))
+  }
+
+  function handleCancelSelectActivity() {
+    setSelectedActivity(undefined);
+  }
+
+  function handlerFormOpen(id?: string) {
+    id ? handleSelectActivity(id): handleCancelSelectActivity();
+    setEditMode(true);
+  }
+
+  function handlerFormClose() {
+    setEditMode(false);
+  }
+
+  function handleCreateOrEditActivity(activity: Activity) {
+    activity.id ? setActivities([...activities.filter(x => x.id !== activity.id), activity])
+    : setActivities([...activities, {...activity, id: uuid()}]);
+    setEditMode(false);
+    setSelectedActivity(activity);
+  }
+
+  function handleDeleteActivity(id: string) {
+    setActivities([...activities.filter(x => x.id !== id)])
+  }
+
   return (
-    <div>
-      <NavBar />
+    <>
+      <NavBar openForm={handlerFormOpen}/>
       <Container style = {{marginTop: '7em'}}>
-      <List>
-      {activities.map(activity => (
-            <List.Item key = {activity.id}>
-              {activity.title}
-            </List.Item>
-          ))}
-      </List>
+      <ActivityDashboard 
+        activities = {activities}
+        selectedActivity={selectedActivity}
+        selectActivity={handleSelectActivity}
+        cancelSelectActivity={handleCancelSelectActivity} 
+        editMode={editMode}
+        openForm={handlerFormOpen}
+        closeForm={handlerFormClose}
+        createOrEdit={handleCreateOrEditActivity}
+        deleteActivity={handleDeleteActivity}
+      />
       </Container>
       
-    </div>
+    </>
   );
 }
 
