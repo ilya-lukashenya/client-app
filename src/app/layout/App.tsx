f9397700f01057ed3c080be5df66a853f89852cd
ilya-lukashenya <il.lukashenya@gmail.com>
2022-08-03 19:48:51 +0300

    Error search.

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -5,9 +5,10 @@ import ActivityDashboard from '../../features/activities/dashboard/ActivityDashb
 import LoadingComponent from './LoadingComponent';
 import { useStore } from '../stores/store';
 import { observer } from 'mobx-react-lite';
-import { Route } from 'react-router-dom';
 import HomePage from '../../features/home/HomePage';
 import ActivityForm from '../../features/activities/form/ActivityForm';
+import ReactDOM from "react-dom/client";
+import { Route } from "react-router-dom";
 
 function App() {
   const {activityStore} = useStore();
@@ -22,9 +23,9 @@ function App() {
     <>
       <NavBar />
       <Container style = {{marginTop: '7em'}}>
-        <Route path='/' component={HomePage} />
-        <Route path='/activities' component={ActivityDashboard} />
-        <Route path='/createActivity' component={ActivityForm} />
+      <Route path='/' element={<HomePage />} />
+        <Route path='/activities' element={<ActivityDashboard />} />
+        <Route path='/createActivity' element={<ActivityForm />} />
       </Container>
       
     </>
